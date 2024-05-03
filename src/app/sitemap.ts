d38d3b4c74ceeeb9995c803371d8996625b27b55
Anil_barry <annenex.11@gmail.com>
2024-05-03 16:57:20 +0530

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,48 @@
 import { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://www.shopntrolly.com/in',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://www.shopntrolly.com/in/store',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.shopntrolly.com/in/products/',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-  ]
+    return [
+        {
+            url: 'https://www.shopntrolly.com/in',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/store',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/products/',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/collections/',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/account/',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/checkout',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://www.shopntrolly.com/in/cart',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+    ]
 }
